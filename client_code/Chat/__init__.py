83a59bd1758cc4483a016037f28ec1ff745aae27
skr.jor@gmail.com <skr.jor@gmail.com>
2024-10-20 19:08:44 +0000

    Created Chat; Deleted Form1

--- client_code/Chat/__init__.py
+++ client_code/Chat/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import ChatTemplate
 from anvil import *
 import anvil.server
 import anvil.users
@@ -7,7 +7,7 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class Chat(ChatTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
